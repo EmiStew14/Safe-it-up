0d0f65b1b8626a2d0bdc24edfe89ede21c1b5e5f
Emily Stewart <stewartemilym2014@gmail.com>
2021-02-20 10:04:00 -0700

    changed alerts to confirms and added messages to console.logs

--- script.js
+++ script.js
@@ -9,29 +9,43 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+var lowcase = "abcdefghijklmnopqrstuvwxyz"
+var upcase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var special = "!@#$%^&*()_+-={}:'<>?[/].,;"
+var number = Math.floor(Math.random());
+
   var charlength = window.prompt("How many characters should be in this password?");
     if (
       (charlength >=8 || charlength <=128) 
     )
     if (
       charlength <8 || charlength >128 ) {
-        charlength.preventDefault();
       window.alert('Password length must be between 8 and 128 characters')
+      return
       }
 
-  console.log ("Password is" + charlength + "characters long");
+  console.log ("Password is " + charlength + " characters long");
 
-  window.alert("Would you like any special characters?");
-  console.log();
+  var specialchar = window.confirm("Would you like any special characters?");
+      if (specialchar) {
+        console.log ('Password will contain special characters')
+      };
 
-  window.alert("Do you want lower cased letters?");
-  console.log();
+  var lowerc = window.confirm("Do you want lower cased letters?");
+    if (lowerc) {
+      console.log ('Password will contain lower cased letters')
+    };
 
-  window.alert("Do you want upper cased letters?");
-  console.log();
+  var upperc = window.confirm("Do you want upper cased letters?");
+  if (upperc) {
+    console.log ('Password will contain upper cased letters')
+  };
 
-  window.alert("Do you want numbers?");
-  console.log();
+  var num = window.confirm("Do you want numbers?");
+  if (num) {
+    console.log ('Password will contain numbers')
+  };
 
   //var password = generatePassword();
   var passwordText = document.querySelector("#password");
