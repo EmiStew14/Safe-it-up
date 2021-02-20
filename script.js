96984fd087c5d58dab8d0095167c9a8ce339af63
Emily Stewart <stewartemilym2014@gmail.com>
2021-02-20 15:52:09 -0700

    added master string shuffle and rearranged some elements to read better

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 // Assignment code here
-var lowcase = "abcdefghijklmnopqrstuvwxyz"
-var upcase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var special = "!@#$%^&*()_+-={}:'<>?[/].,;"
-var number = Math.floor(Math.random());
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,11 +6,11 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
 
-var lowcase = "abcdefghijklmnopqrstuvwxyz"
-var upcase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var special = "!@#$%^&*()_+-={}:'<>?[/].,;"
-var number = Math.floor(Math.random());
-
+var lowerc = "abcdefghijklmnopqrstuvwxyz"
+var upperc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var specialchar = "!@#$%^&*()_+-={}:'<>?[/].,;"
+var num = "0123456789"
+ 
   var charlength = window.prompt("How many characters should be in this password?");
     if (
       (charlength >=8 || charlength <=128) 
@@ -30,28 +26,60 @@ var number = Math.floor(Math.random());
   var specialchar = window.confirm("Would you like any special characters?");
       if (specialchar) {
         console.log ('Password will contain special characters')
+
+      }else {
+        console.log("false")
       };
 
   var lowerc = window.confirm("Do you want lower cased letters?");
     if (lowerc) {
       console.log ('Password will contain lower cased letters')
+    
+    }else {
+      console.log("false")
     };
 
   var upperc = window.confirm("Do you want upper cased letters?");
   if (upperc) {
     console.log ('Password will contain upper cased letters')
+  
+  }else {
+    console.log("false")
   };
 
   var num = window.confirm("Do you want numbers?");
   if (num) {
     console.log ('Password will contain numbers')
+
+  }else {
+    console.log("false")
   };
 
-  //var password = generatePassword();
+  if (specialchar + lowerc + upperc + num === 'false'); {
+  window.alert = "You must pick a category! Please choose at lease one."
+  return
+  };
+};
+
+  function pwdShuffle (mastStr) {
+    var arr = mastStr.split('');
+
+    arr.sort(function() {
+      return 0.5 - Math.random();
+    });
+    mastStr = arr.join('');
+    return mastStr;
+  }
+  var mastStr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+-={}:'<>?[/].,;0123456789";
+  mastStr = pwdShuffle(mastStr);
+
+  /*console.log(mastStr);*/
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  var writePassword = (password);
 
-    };
-// Add event listener to generate button
+  function writePassword(pwdShuffle)
+  
 generateBtn.addEventListener("click", writePassword);
